066de122c2218a6a3dc57e5861f855c26191b563
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-04-13 17:22:19 -0300

    feat: componentizing header

---
+++ src/components/structures/Header/index.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+export const Header = ({ user, setUser, isLoginOpen, setIsLoginOpen }) => {
+   const userLogout = () => {
+      setUser(null);
+      localStorage.removeItem("@TOKEN");
+   };
+
+   return (
+      <header>
+         {user ? (
+            <div>
+               <p>{user.name}</p>
+               <p>{user.email}</p>
+               <button onClick={() => userLogout()}>Sair</button>
+            </div>
+         ) : (
+            <div>
+               <button onClick={() => setIsLoginOpen(true)}>Entrar</button>
+               <Link to="/register">Cadastrar-se</Link>
+               {isLoginOpen ? (
+                  <div>
+                     <button onClick={() => setIsLoginOpen(false)}>Fechar</button>
+                     <LoginForm setUser={setUser} setIsLoginOpen={setIsLoginOpen} />
+                  </div>
+               ) : null}
+            </div>
+         )}
+      </header>
+   );
+};
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,35 +1,18 @@
 import { useState } from "react";
+import { Header } from "../../components/structures/Header";
 import { LoginForm } from "../../components/structures/LoginForm";
 
 export const HomePage = ({ user, setUser }) => {
    const [isLoginOpen, setIsLoginOpen] = useState(false);
 
-   const userLogout = () => {
-    setUser(null);
-    localStorage.removeItem("@TOKEN");
-   }
-
    return (
       <div>
-         <header>
-            {user ? (
-               <div>
-                <p>{user.name}</p>
-                <p>{user.email}</p>
-                <button onClick={() => userLogout()}>Sair</button>
-               </div>
-            ) : (
-               <div>
-                  <button onClick={() => setIsLoginOpen(true)}>Entrar</button>
-                  {isLoginOpen ? (
-                     <div>
-                        <button onClick={() => setIsLoginOpen(false)}>Fechar</button>
-                        <LoginForm setUser={setUser} setIsLoginOpen={setIsLoginOpen} />
-                     </div>
-                  ) : null}
-               </div>
-            )}
-         </header>
+         <Header
+            user={user}
+            setUser={setUser}
+            isLoginOpen={isLoginOpen}
+            setIsLoginOpen={setIsLoginOpen}
+         />
       </div>
    );
 };

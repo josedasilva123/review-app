e8775b375fa4c1ae26595ab7feb9ebd2c4166851
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-04-13 17:22:29 -0300

    feat: adding back link

--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom"
 import { RegisterForm } from "../../components/structures/RegisterForm"
 
 export const RegisterPage = () => {
     return(
         <div>
+            <Link to="/">Voltar</Link>
             <RegisterForm />
         </div>
     )

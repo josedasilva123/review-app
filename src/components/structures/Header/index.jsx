6398984afb465e39d41ca30cb6d811cc0f1d6848
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-04-13 17:23:27 -0300

    fix: adding missing import

--- src/components/structures/Header/index.jsx
+++ src/components/structures/Header/index.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { LoginForm } from "../LoginForm";
 
 export const Header = ({ user, setUser, isLoginOpen, setIsLoginOpen }) => {
    const userLogout = () => {
